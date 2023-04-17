9a108b3d80abf3d54ac37600b8a12c13b18d931d
Nischal Pandey <admin@nischalpandey.com.np>
2023-04-17 11:19:21 +0545

    Update settings.py

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -183,8 +183,8 @@ STATICFILES_FINDERS = [
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = 'assets/media/'
-MEDIA_ROOT = 'staticfiless'
+MEDIA_URL = 'https://cdn.nischalpandey.com.np/media/'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
